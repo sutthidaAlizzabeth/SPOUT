4689c2c1cd1367fa0e689a4dfb395fbad614e2cb
Alizzabeth <Alizzabeth@sutthidas-MacBook-Pro.local>
2016-08-26 18:44:38 +0700

    for Tan

---
+++ SPOUT/Assets/Script/ChangeScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	// Use this for change scene
+	public void Change (string sceneName) {
+		Application.LoadLevel(sceneName);
+	}
+}
