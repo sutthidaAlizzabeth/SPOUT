8f0f3d5f16244980ebc57b0a2c24597e71d4e712
sutthidaAlizzabeth <sutthida.alizzabeth@gmail.com>
2016-08-29 02:05:16 +0700

    script GoBack.cs: dynamic methode

--- SPOUT/Assets/Script/GoBack.cs
+++ SPOUT/Assets/Script/GoBack.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class GoBack : MonoBehaviour {
  	
+	public string sceneName;
 
 	// Use this for back to pre scene
 	public void Update () {
 		if (Input.GetKey (KeyCode.Escape)) {
-			Application.LoadLevel("sc_main");
+			Application.LoadLevel(sceneName);
 		}
 	}
 }
