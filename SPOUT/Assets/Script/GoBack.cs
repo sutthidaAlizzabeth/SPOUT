2c6d50a601d26abe676e98c0f8141e370fb5a683
sutthidaAlizzabeth <sutthida.alizzabeth@gmail.com>
2016-08-29 01:45:56 +0700

    main menu: change sprite to button 2D

--- SPOUT/Assets/Script/GoBack.cs
+++ SPOUT/Assets/Script/GoBack.cs
@@ -3,12 +3,11 @@ using System.Collections;
 
 public class GoBack : MonoBehaviour {
  	
-	public string sceneName;
 
 	// Use this for back to pre scene
-	public void Back () {
+	public void Update () {
 		if (Input.GetKey (KeyCode.Escape)) {
-			Application.LoadLevel(sceneName);
+			Application.LoadLevel("game_theme");
 		}
 	}
 }
