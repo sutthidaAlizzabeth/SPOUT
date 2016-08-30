37ed86ccc8ca40760d9a32fd11257906a8b73a54
sutthidaAlizzabeth <sutthida.alizzabeth@gmail.com>
2016-08-30 23:41:48 +0700

    initial for PUT

---
+++ SPOUT/Assets/Script/recognition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class recognition : MonoBehaviour {
+
+	public InputField ShowText;
+
+	public void Start()
+	{
+		ShowText.text = "Alizzabet";
+	}
+
+	//when click microphone button
+	void Update () {
+	
+	}
+}
