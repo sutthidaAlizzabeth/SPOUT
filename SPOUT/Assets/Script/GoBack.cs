1bb196369738418dbb5eafc801a1ccd70b43768c
sutthidaAlizzabeth <sutthida.alizzabeth@gmail.com>
2016-08-30 09:08:48 +0700

    use new aggreated script with each scene

--- SPOUT/Assets/Script/GoBack.cs
+++ SPOUT/Assets/Script/GoBack.cs
@@ -1,14 +1,25 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class GoBack : MonoBehaviour {
  	
+	//this parameter derive from each scene
 	public string sceneName;
 
-	// Use this for back to pre scene
-	public void Update () {
+	// Update is called once per frame
+	void Update () {
+		//when user touch "back" button
 		if (Input.GetKey (KeyCode.Escape)) {
-			Application.LoadLevel(sceneName);
+			//if sceneName equals "quit"
+			//user touch "back" button in sc_main
+			//user want to close app
+			if (String.Compare (sceneName, "quit") == 0) {
+				Application.Quit();
+			} else {
+				//load scene
+				Application.LoadLevel(sceneName);
+			}
 		}
 	}
 }
