3336f5c99b64978051adccdfc486b4d1ef0d396e
sutthidaAlizzabeth <sutthida.alizzabeth@gmail.com>
2016-08-31 08:31:51 +0700

    add script to show text in InputField

---
+++ SPOUT/Assets/Script/recognition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class recognition : MonoBehaviour {
+
+	public InputField field;
+
+	// Use this for initialization
+	public void Start () {
+		field.text = "Alizzabeth";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
