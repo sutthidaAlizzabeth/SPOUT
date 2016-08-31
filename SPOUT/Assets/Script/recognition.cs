7f2f9af4974e7bbb87fd9d2c299cee4c5596802a
sutthidaAlizzabeth <sutthida.alizzabeth@gmail.com>
2016-08-31 10:41:00 +0700

    start using AndroidJavaClass and AndroidJavaObject and call android.speech.RecognizerIntent and android.content.Intent

--- SPOUT/Assets/Script/recognition.cs
+++ SPOUT/Assets/Script/recognition.cs
@@ -9,6 +9,10 @@ public class recognition : MonoBehaviour {
 	// Use this for initialization
 	public void Start () {
 		field.text = "Alizzabeth";
+
+		AndroidJavaClass s = new AndroidJavaClass ("android.speech.RecognizerIntent");
+		AndroidJavaObject intent = new AndroidJavaObject ("android.content.Intent", s.GetStatic<string>("ACTION_RECOGNIZE_SPEECH"));
+
 	}
 	
 	// Update is called once per frame
