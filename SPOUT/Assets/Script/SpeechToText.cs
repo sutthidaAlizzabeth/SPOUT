2a7f3492b2339f23ced54948c8903f8eebe59b7e
sutthidaAlizzabeth <sutthida.alizzabeth@gmail.com>
2016-09-02 00:32:03 +0700

    add new script for recognition scene

--- SPOUT/Assets/Script/SpeechToText.cs
+++ SPOUT/Assets/Script/SpeechToText.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class SpeechToText : MonoBehaviour {
 
+	public InputField field;
+
 	// Use this for initialization
 	void Start () {
-	
+		field.text = "Show Text Here!";
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void OnSpeech(string str){
+		field.text = str;
 	}
 }
