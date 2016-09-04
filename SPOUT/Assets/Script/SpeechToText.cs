e5542a0ab0be9304ec607d6c1b20e4398a64d704
sutthidaAlizzabeth <sutthida.alizzabeth@gmail.com>
2016-09-04 18:19:33 +0700

    set language US

--- SPOUT/Assets/Script/SpeechToText.cs
+++ SPOUT/Assets/Script/SpeechToText.cs
@@ -28,7 +28,16 @@ public class SpeechToText : MonoBehaviour {
 		if (isRecording) {
 			// create the intent and start the activity
 			var voiceIntent = new Intent (RecognizerIntent.ActionRecognizeSpeech);
-			voiceIntent.PutExtra (RecognizerIntent.ExtraLanguageModel, RecognizerIntent.LanguageModelFreeForm);
+			voiceIntent.PutExtra (RecognizerIntent.ExtraLanguageModel, "en-US");
+
+			// put a message on the modal dialog
+			voiceIntent.PutExtra(RecognizerIntent.ExtraPrompt, "Allizabeth");
+
+			// if there is more then 1.5s of silence, consider the speech over
+			voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputCompleteSilenceLengthMillis, 1500);
+			voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputPossiblyCompleteSilenceLengthMillis, 1500);
+			voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputMinimumLengthMillis, 15000);
+			voiceIntent.PutExtra(RecognizerIntent.ExtraMaxResults, 1);
 
 			//change source image of button to stop recording button
 			btnRecord.image.overrideSprite = imgStop;
