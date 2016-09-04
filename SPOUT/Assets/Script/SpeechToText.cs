a8b20e7eadeadda4786b177a03c0e74b6fe9ee02
sutthidaAlizzabeth <sutthida.alizzabeth@gmail.com>
2016-09-04 21:33:48 +0700

    ::Not Test:: finnished speech to text

--- SPOUT/Assets/Script/SpeechToText.cs
+++ SPOUT/Assets/Script/SpeechToText.cs
@@ -21,9 +21,6 @@ public class SpeechToText : MonoBehaviour {
 	
 	//Use this when click microphone button
 	public void OnSpeech(string str){
-		//set text in InputField
-		field.text = str;
-
 		isRecording = !isRecording;
 		if (isRecording) {
 			// create the intent and start the activity
@@ -38,6 +35,23 @@ public class SpeechToText : MonoBehaviour {
 			voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputPossiblyCompleteSilenceLengthMillis, 1500);
 			voiceIntent.PutExtra(RecognizerIntent.ExtraSpeechInputMinimumLengthMillis, 15000);
 			voiceIntent.PutExtra(RecognizerIntent.ExtraMaxResults, 1);
+
+			//result
+			var matches = voiceIntent.GetStringArrayListExtra(RecognizerIntent.ExtraResults);
+			if(matches.Count != 0){
+				string textInput = field.text + matches [0];
+
+				// limit the output to 500 character
+				if (textInput.Length > 500) {
+					textInput = textInput.Substring (0, 500);
+				}
+
+				//set content in "field"
+				field.text = textInput;
+			}
+			else{
+				field.text = "No speech was recognised";
+			}
 
 			//change source image of button to stop recording button
 			btnRecord.image.overrideSprite = imgStop;
