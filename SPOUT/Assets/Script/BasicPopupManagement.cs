2c858631378479efebf538f468ac5ad6f9f4b9c6
sutthidaAlizzabeth <sutthida.alizzabeth@gmail.com>
2016-09-12 16:54:35 +0700

    add script to manage popup

---
+++ SPOUT/Assets/Script/BasicPopupManagement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicPopupManagement : MonoBehaviour {
+	public Canvas popupCanvas;
+
+	void Start(){
+		popupCanvas.enabled = false;
+	}
+	
+	// Update is called once per frame
+	public void ShowPopup () {
+		popupCanvas.enabled = true;
+	}
+}
