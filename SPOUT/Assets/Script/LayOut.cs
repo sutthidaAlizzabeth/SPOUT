20b545047f426a36045c83f2858a65c9c45cce7b
sutthidaAlizzabeth <sutthida.alizzabeth@gmail.com>
2016-09-17 03:04:25 +0700

    ::merge game__levle::

---
+++ SPOUT/Assets/Script/LayOut.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LayOut : MonoBehaviour {
+	public string orientation = "";
+	// Use this for initialization
+	void Start () {
+		if (orientation == "portrait") {
+			Screen.orientation = ScreenOrientation.Portrait;
+		}else if(orientation == "landscape"){
+			Screen.orientation = ScreenOrientation.Landscape;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
