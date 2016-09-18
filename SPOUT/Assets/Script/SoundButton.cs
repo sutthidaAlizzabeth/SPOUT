2ff940ea12af43dad3cba7b71552a956e4badf3c
sutthidaAlizzabeth <sutthida.alizzabeth@gmail.com>
2016-09-18 23:08:09 +0700

    up to date

---
+++ SPOUT/Assets/Script/SoundButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundButton : MonoBehaviour {
+	private AudioSource sound;
+
+	public void playEffect(){
+		sound = new AudioSource ();
+		sound.clip = Resources.Load ("rec", typeof(AudioClip)) as AudioClip;
+		sound.Play ();
+	}
+}
